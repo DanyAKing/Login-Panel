e895e422c946ef63eb455f13b264c7bb2dfe7048
Dany King <danielkkoper@gmail.com>
2023-03-28 23:06:00 +0200

    configured express-handlebars, created correct structure

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const { loginRedirection } = require('./src/routers/loginRedirection');
 const { loginRouters } = require('./src/routers/loginRouters');
 
@@ -8,17 +8,15 @@ const app = express();
 app.use(express.urlencoded({
   extended: true,
 }));
-app.engine('.hbs', hbs.engine({
-  extname: '.hbs',
-}));
-app.set('view engine', '.hbs');
-// app.engine('hbs', engine());
-// app.set('view engine', 'hbs');
-// app.set('views', './views');
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/', loginRedirection);
 app.use('/login', loginRouters);
+app.engine('hbs', engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.listen(3000, '127.0.0.1', () => {
   console.log('Server started successfully!');
--- src/routers/loginRouters.js
+++ src/routers/loginRouters.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const express = require('express');
 const { readFile } = require('fs').promises;
 // const { hashPassword } = require('../bcrypt/hash');
@@ -5,10 +6,17 @@ const { compareHash } = require('../bcrypt/compareHash');
 
 const loginRouters = express.Router();
 
+const generateKey = () => crypto.randomBytes(16).toString('base64');
+
 loginRouters
+  .get('/', (req, res) => {
+    res.render('login/loginPanel');
+  })
   .post('/', async (req, res) => {
     const getPass = await readFile('data/password.json', 'utf-8');
     compareHash(req.body.password, JSON.parse(getPass));
+    const sessionId = generateKey();
+    res.redirect(`/login/${sessionId}`);
   });
 
 module.exports = { loginRouters };
