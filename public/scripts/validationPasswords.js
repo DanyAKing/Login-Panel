368bdd8382dd16019594b92d8d7c4654677ca780
Dany King <danielkkoper@gmail.com>
2023-06-18 12:46:18 +0200

    validation password (frontend side) works correctly

--- public/scripts/validationPasswords.js
+++ public/scripts/validationPasswords.js
@@ -1,17 +1,11 @@
 const registrationForm = document.getElementById('registration');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirm_password');
-const alert = document.getElementById('password_alert');
+const alert = document.getElementById('password_incorrect');
 
 registrationForm.addEventListener('submit', (event) => {
-  if (passwordInput !== confirmPasswordInput) {
+  if (passwordInput.value !== confirmPasswordInput.value) {
     alert.style.display = 'block';
     event.preventDefault();
-  } else if (passwordInput === confirmPasswordInput) {
-    alert.style.display = 'none';
   }
 });
-
-// passwordInput.addEventListener('click', () => {
-//   alert.style.display = 'none';
-// });
